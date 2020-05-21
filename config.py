d1bf3fc84c361112f30c9bc615c5d8394e68aa18
VladislavKim1 <64124215+VladislavKim1@users.noreply.github.com>
2020-05-22 01:36:04 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+screen_width = 1980
+screen_height = 1200
+
+border_color = "#A52A2A"
+field_color = "#013220"
+
+indent_y = 20
+intent_x = 50
+border_width = 20
+
+field_x = intent_x
+field_y = intent_x + indent_y
+field_width = screen_width - 2 * field_x
+field_height = screen_height - 2 * field_y
+
+last_time = 0
+stop_speed = 0
+max_speed = 3
+acceleration = 500
