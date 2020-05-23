44b5a7299919e3094496c224f64867054ce4c1cc
VladislavKim1 <64124215+VladislavKim1@users.noreply.github.com>
2020-05-23 19:30:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+balls_filename = 'balls.txt'
+
 screen_width = 1980
 screen_height = 1200
 
@@ -15,5 +17,18 @@ field_height = screen_height - 2 * field_y
 
 last_time = 0
 stop_speed = 0
-max_speed = 3
+max_speed = 5
 acceleration = 500
+
+
+def board(canvas):
+    canvas.pack()
+
+    canvas.create_text(screen_width / 2, indent_y,
+                       text="Use WASD to accelerate the white ball, to quit use Q")
+
+    canvas.create_rectangle(field_x, field_y, field_x + field_width, field_y + field_height, fill=border_color,
+                            outline="")
+
+    canvas.create_rectangle(field_x + border_width, field_y + border_width, field_x + field_width - border_width,
+                            field_y + field_height - border_width, fill=field_color, outline="")
