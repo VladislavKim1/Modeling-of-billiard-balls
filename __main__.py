e9a9b05225202110bd87e52afcaa8805cf22851d
VladislavKim1 <64124215+VladislavKim1@users.noreply.github.com>
2020-05-23 19:29:33 +0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,26 +1,34 @@
 import time
 import json
-from math import sqrt, fabs
+from math import sqrt
 from tkinter import *
 
-from config import (last_time, stop_speed, max_speed, acceleration, screen_width, screen_height, border_color,
-                     field_color, indent_y, intent_x, border_width, field_x, field_y, field_width, field_height)
+from config import *
+
+root = Tk()
+root.title("billiard balls")
+balls: list = list()
+
+canvas = Canvas(root, width=screen_width, height=screen_height)
+
+
+def vector_multiplication(a: tuple, b: tuple):
+    return (a[0]*b[0]) + (a[1]*b[1])
 
 
 class Ball:
     """
     This class describes the behavior of the ball.
     """
-
-    m = 0  # weight
     _player: bool = False
 
-    def __init__(self, location: list, color: str = 'black', radius: float = 30.0):
+    def __init__(self, location: list, color: str = 'black', radius: float = 30.0, balls = balls):
         """
         Ball constructor
         """
+        self.color: str = color
         self.radius: float = radius
-        self.m = radius / 10
+        self.m = radius * 10  # weight
         self.location: dict = dict()
         self.location['x'] = location[0]
         self.location['y'] = location[1]  # ball_1_centre_x, y
@@ -28,7 +36,7 @@ class Ball:
                                        self.location['y'] - self.radius,
                                        self.location['x'] + self.radius,
                                        self.location['y'] + self.radius,
-                                       fill=color, outline="")
+                                       fill=self.color, outline="")
         self.speed_x = 0
         self.speed_y = 0
 
@@ -76,7 +84,7 @@ def update():
             dx = ball_one.speed_x * dt
             dy = ball_one.speed_y * dt
 
-            # ограничение скорости
+            # speed limit
             if abs(dx) > max_speed:
                 if dx > 0:
                     dx = max_speed
@@ -89,40 +97,36 @@ def update():
                 elif dy < 0:
                     dy = -max_speed
 
-            # затухание
-            #if ball_one.speed_y > 0:
-               # ball_one.speed_y -= stop_speed
-
-            #if ball_one.speed_x > 0:
-               # ball_one.speed_x -= stop_speed
-
-            #if ball_one.speed_y < 0:  # revers
-              #  ball_one.speed_y += stop_speed
-
-           # if ball_one.speed_x < 0:
-             #   ball_one.speed_x += stop_speed
+            # attenuation
+            ball_one.speed_y /= 1 + stop_speed
+            ball_one.speed_x /= 1 + stop_speed
 
             buf_balls: list = balls.copy()
             del buf_balls[index]
 
             for ball_two in buf_balls:
                 strike: bool = False
+                R = ball_one.radius + ball_two.radius
+
+                max_x = field_x + field_width - border_width - ball_one.radius
+                max_y = field_y + field_height - border_width - ball_one.radius
+                min_x = field_x + border_width + ball_one.radius
+                min_y = field_y + border_width + ball_one.radius
+
+                if not (min_x <= ball_one.location['x'] + dx <= max_x):
+                    ball_one.speed_x = -ball_one.speed_x
+                    strike = True
+                if not (min_y <= ball_one.location['y'] + dy <= max_y):
+                    ball_one.speed_y = -ball_one.speed_y
+                    strike = True
 
                 sigma_next = sqrt(pow(((ball_one.location['x'] + dx) - ball_two.location['x']), 2) + pow(((ball_one.location['y'] + dy) - ball_two.location['y']), 2))
-                if sigma_next <= (ball_one.radius + ball_two.radius):
+                if sigma_next <= R:
                     strike = True
-                    dx = 0
-                    dy = 0
-
-                sigma = sqrt(pow((ball_one.location['x'] - ball_two.location['x']), 2) + pow((ball_one.location['y'] - ball_two.location['y']), 2))
-                if sigma <= (ball_one.radius + ball_two.radius):
-                    strike = True
-
-                if strike:
+                    sigma = sqrt(pow((ball_one.location['x'] - ball_two.location['x']), 2) + pow((ball_one.location['y'] - ball_two.location['y']), 2))
                     dr: tuple = (ball_two.location['x'] - ball_one.location['x'], ball_two.location['y'] - ball_one.location['y'])
                     du: tuple = (ball_two.speed_x - ball_one.speed_x, ball_two.speed_y - ball_one.speed_y)
-                    vector_multiplication = (du[0]*dr[0]) + (du[1]*dr[1])
-                    J = (2 * ball_one.m * ball_two.m * vector_multiplication) / (sigma * (ball_one.m + ball_two.m))
+                    J = (2 * ball_one.m * ball_two.m * vector_multiplication(du, dr)) / (sigma * (ball_one.m + ball_two.m))
 
                     drx = ball_two.location['x'] - ball_one.location['x']
                     dry = ball_two.location['y'] - ball_one.location['y']
@@ -136,21 +140,13 @@ def update():
                     ball_two.speed_x -= Jx / ball_two.m
                     ball_two.speed_y -= Jy / ball_two.m
 
+                if strike:
+                    # Skip to check for collisions in the next iteration
+                    dx = 0
+                    dy = 0
+
             ball_one.location['x'] += dx
             ball_one.location['y'] += dy
-
-            max_x = field_x + field_width - border_width - ball_one.radius
-            max_y = field_y + field_height - border_width - ball_one.radius
-            min_x = field_x + border_width + ball_one.radius
-            min_y = field_y + border_width + ball_one.radius
-
-            if not (min_x <= ball_one.location['x'] <= max_x):
-                ball_one.location['x'] = max(min_x, min(ball_one.location['x'], max_x))
-                ball_one.speed_x = -ball_one.speed_x
-            if not (min_y <= ball_one.location['y'] <= max_y):
-                ball_one.location['y'] = max(min_y, min(ball_one.location['y'], max_y))
-                ball_one.speed_y = -ball_one.speed_y
-
             ball_one.render()
 
     last_time = cur_time
@@ -158,32 +154,8 @@ def update():
 
 
 if __name__ == '__main__':
-    root = Tk()
-    root.title("billiard balls")
-
-    canvas = Canvas(root, width=screen_width, height=screen_height)
-    canvas.pack()
-
-    text = canvas.create_text(screen_width / 2, indent_y,
-                              text="Use WASD to accelerate the white ball, to quit use Q")
-
-    border = canvas.create_rectangle(field_x, field_y, field_x + field_width, field_y + field_height, fill=border_color,
-                                     outline="")
-
-    field = canvas.create_rectangle(field_x + border_width, field_y + border_width, field_x + field_width - border_width,
-                                    field_y + field_height - border_width, fill=field_color, outline="")
-
-    #balls: list = list()
-    #with open('balls.txt') as f:
-        #for ball_name, struct in json.load(f).items():
-            #balls.append(Ball(struct['position'], struct['color'], struct['radius']))
-
-    #balls: list = list()
-    #with open('100_balls.txt') as f:
-        #for ball_name, struct in json.load(f).items():
-            #balls.append(Ball(struct['position'], struct['color'], struct['radius']))
-    balls: list = list()
-    with open('billiard.txt') as f:
+    board(canvas)
+    with open(balls_filename) as f:
         for ball_name, struct in json.load(f).items():
             balls.append(Ball(struct['position'], struct['color'], struct['radius']))
 
@@ -191,3 +163,4 @@ if __name__ == '__main__':
 
     update()
     root.mainloop()
+
