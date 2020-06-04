05c975a85985738972bbb3b343519ffaaa683efe
VladislavKim1 <64124215+VladislavKim1@users.noreply.github.com>
2020-06-04 11:19:36 +0700

    Create config_test.py

---
+++ config_test.py
@@ -0,0 +1,21 @@
+balls_filename = 'balls.json'
+
+screen_width = 1200
+screen_height = 800
+
+border_color = "#A52A2A"
+field_color = "#013220"
+
+indent_y = 20
+intent_x = 50
+border_width = 20
+
+field_x = intent_x
+field_y = intent_x + indent_y
+field_width = screen_width - 2 * field_x
+field_height = screen_height - 2 * field_y
+
+last_time = 0
+stop_speed = 0.004
+max_speed = 5
+acceleration = 500
