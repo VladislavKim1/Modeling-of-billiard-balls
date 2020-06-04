2f3592df6e80e8512a4d8a0d8fdc814e2215abe9
VladislavKim1 <64124215+VladislavKim1@users.noreply.github.com>
2020-06-04 11:17:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-balls_filename = 'balls.txt'
+balls_filename = 'billiard.json'
 
 screen_width = 1980
 screen_height = 1200
@@ -15,21 +15,16 @@ field_y = intent_x + indent_y
 field_width = screen_width - 2 * field_x
 field_height = screen_height - 2 * field_y
 
+hole_cord: list = [
+    (field_x + border_width + 10, field_y + border_width + 10),
+    ((field_x + border_width + field_x + field_width - border_width) / 2, field_y + border_width),
+    (field_x + field_width - border_width - 10, field_y + border_width + 10),
+    (field_x + border_width + 10, field_y + field_height - border_width - 10),
+    ((field_x + border_width + field_x + field_width - border_width) / 2, field_y + field_height - border_width),
+    (field_x + field_width - border_width - 10, field_y + field_height - border_width - 10),
+]
+
 last_time = 0
-stop_speed = 0 #ball deceleration coefficient
-#stop_speed = 0.0005
+stop_speed = 0.0009
 max_speed = 5
 acceleration = 500
-
-
-def board(canvas):
-    canvas.pack()
-
-    canvas.create_text(screen_width / 2, indent_y,
-                       text="Use WASD to accelerate the white ball, to quit use Q")
-
-    canvas.create_rectangle(field_x, field_y, field_x + field_width, field_y + field_height, fill=border_color,
-                            outline="")
-
-    canvas.create_rectangle(field_x + border_width, field_y + border_width, field_x + field_width - border_width,
-                            field_y + field_height - border_width, fill=field_color, outline="")
